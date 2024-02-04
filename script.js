90ed3a29fd0a39747072bd7f9b0276e079e45e51
Debanjan Mondal <debanjanmondalk2005@gmail.com>
2024-02-04 18:59:43 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+function toggleDropdown() {
+    const dropdown = document.querySelector('#dropdown-menu');
+    dropdown.classList.toggle('hidden');}
+    function toggleDropdown1() {
+        const dropdown = document.querySelector('#dropdown1-menu');
+        dropdown.classList.toggle('hidden');}
+        function toggleDropdown2() {
+            const dropdown = document.querySelector('#dropdown2-menu');
+            dropdown.classList.toggle('hidden');}
+            function toggleDropdown3() {
+                const dropdown = document.querySelector('#dropdown3-menu');
+                dropdown.classList.toggle('hidden');}
+                function toggleDropdown4() {
+                    const dropdown = document.querySelector('#dropdown4-menu');
+                    dropdown.classList.toggle('hidden');}
+                    function toggleDropdown5() {
+                        const dropdown = document.querySelector('#dropdown5-menu');
+                        dropdown.classList.toggle('hidden');}
+                        function toggleDropdown6() {
+                            const dropdown = document.querySelector('#dropdown6-menu');
+                            dropdown.classList.toggle('hidden');}
